104d6d711511d9596d479fb084aeecc6fc41a4e4
Ben Dunham <ben.dunham@theo.agency>
2022-09-27 15:57:00 -0700

    updating with sanity

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { Project } from "../typings";
+import { urlFor } from "../sanity";
 
-type Props = {};
+type Props = {
+  projects: Project[];
+};
 
-function Projects({}: Props) {
-  const projects = [1, 2, 3, 4, 5];
+function Projects({ projects }: Props) {
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -17,7 +20,7 @@ function Projects({}: Props) {
       </h3>
 
       <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
-        {projects.map((project, i) => (
+        {projects?.map((project, i) => (
           <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
             <motion.img
               initial={{
@@ -27,7 +30,7 @@ function Projects({}: Props) {
               transition={{ duration: 1.2 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
-              src='https://netflix-clone-82480.web.app/section3img.png'
+              src={urlFor(project?.image).url()}
               alt=''
             />
 
@@ -36,17 +39,22 @@ function Projects({}: Props) {
                 <span className='underline decoration-[#f7ab0a]/50'>
                   Case Study {i + 1} of {projects.length}:
                 </span>{" "}
-                UPS clone
+                {project?.title}
               </h4>
 
+              <div className='flex items-center space-x-2 justify-center'>
+                {project?.technologies.map((technology) => (
+                  <img
+                    className='h-10 w-10'
+                    key={technology._id}
+                    src={urlFor(technology.image).url()}
+                    alt=''
+                  />
+                ))}
+              </div>
+
               <p className='text-lg text-center md:text-left'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed
-                magni pariatur delectus fugit, quo deserunt dicta quibusdam fuga
-                temporibus alias eius sit earum ab error accusamus voluptatibus
-                provident tempore voluptas molestiae quasi atque a. Corrupti,
-                excepturi sapiente? Sit laboriosam quidem harum veritatis
-                consectetur molestias, cumque sapiente porro necessitatibus cum
-                molestiae.
+                {project?.summary}
               </p>
             </div>
           </div>
